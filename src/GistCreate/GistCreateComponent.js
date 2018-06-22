c33bf7aa3a64216d4f8cf48f75143a16e326352e
SreeraG <sreerag.nair@built.io>
2018-06-22 11:50:14 +0530

    Serverless approach not working properly it seems

--- src/GistCreate/GistCreateComponent.js
+++ src/GistCreate/GistCreateComponent.js
@@ -1,45 +1,94 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Divider from '@material-ui/core/Divider'
-class GistCreateComponent extends Component{
+import { TextField, Switch } from '@material-ui/core';
+class GistCreateComponent extends Component {
 
 
     state = {
-        code : '',
-        files : '',
-        description : 'THIS IS A TEST DESCRIPTION',
-        public : false,
+        access_token: '',
+        gist_name: '',
+        gist_content: '',
+        public: false,
+        description: '',
     }
-    
-    componentDidMount(){
+
+    componentDidMount() {
         // alert('GistCreateComponent')
-        const code =
+        const access_token =
             window.location.href.match(/\?code=(.*)/) &&
             window.location.href.match(/\?code=(.*)/)[1]
     }
-    
-    render(){
-        return(
-            
+
+    handleChange(event){
+            this.setState({ [event.target.name] : event.target.value })
+    }
+
+    handleSwitchChange(event){
+        this.setState({ [event.target.name] : !this.state.public })
+    }
+
+    render() {
+
+       
+
+        return (
+
             <div>
-                
+
                 <h1>GistCreateComponent</h1>
 
                 <Divider />
-                <div style = {{ width : '100%', height : '300px', background : 'yellow', }} >
+                <div style={{ width: '100%', height: '300px', background: 'azure', }} >
+
+                    <TextField id="name"
+                        name = "gist_name"  
+                        label="Gist name"
+                        value={this.state.gist_name}
+                        onChange={this.handleChange.bind(this)}
+                        margin="normal" />
+                    
+                    <TextField id="content"
+                        name = "gist_content"  
+                        label="Gist content"
+                        value={this.state.gist_content}
+                        onChange={this.handleChange.bind(this)}
+                        margin="normal" />
+                    
+                    <Switch
+                    checked = { this.state.public }
+                    onChange = { this.handleSwitchChange.bind(this) }
+                    name = "public"
+                    />
+
+                    <TextField id="description"
+                        name = "description"  
+                        label="Description"
+                        value={this.state.description}
+                        onChange={this.handleChange.bind(this)}
+                        margin="normal" />
+
+
+
                 </div>
                 <Divider />
-                <div style = {{ width : '100%', height : '300px', background : 'teal', marginTop : '20px' }}>
-                <p>{'{'}</p>
-                <p style = {{ marginLeft : '40px' }}>Sreerag</p>
-                <p style = {{ marginLeft : '40px' }}>Sreerag</p>
-                <p style = {{ marginLeft : '40px' }}>public : 
-                        <p style = {{ marginLeft : '60px' }}>{ this.state.public.toString() }</p>
-                </p>
-                <p style = {{ marginLeft : '40px' }}>description : 
-                        <p style = {{ marginLeft : '60px' }}>{ this.state.description }</p>
-                </p>
-                <p>{'}'}</p>
+
+                <div style={{ width: '100%', height: '300px', background: 'teal', marginTop: '20px' }}>
+                    <p>{'{'}</p>
+
+                    <div style={{ marginLeft: '40px' }}>Gist name (required) :
+                        <p style={{ marginLeft: '60px' }}>{this.state.gist_name || 'Enter a name for the gist'}</p>
+                    </div>
+                    <div style={{ marginLeft: '40px' }}>Gist content (required) :
+                        <p style={{ marginLeft: '60px' }}>{this.state.gist_content || 'Enter something here'}</p>
+                    </div>
+                    <div style={{ marginLeft: '40px' }}>public :
+                        <p style={{ marginLeft: '60px' }}>{this.state.public.toString()}</p>
+                    </div>
+                    <div style={{ marginLeft: '40px' }}>description :
+                        <p style={{ marginLeft: '60px' }}>{this.state.description || 'Enter a short description'}</p>
+                    </div>
+                    <p>{'}'}</p>
                 </div>
 
             </div>
--- src/RedirectComponent.js
+++ src/RedirectComponent.js
@@ -8,24 +8,65 @@ class RedirectComponent extends Component {
         code: '',
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const code =
             window.location.href.match(/\?code=(.*)/) &&
             window.location.href.match(/\?code=(.*)/)[1]
         this.setState({ code: code })
 
+
+
+    }
+
+    componentDidMount() {
+
+        /* 
         setTimeout(() =>{
 
             this.props.history.push('/d?access_token=' + this.state.code)
 
-        }, 3000)
+        }, 10000)
+        */
+
+        var githubAuthObj = {
+            client_id: '3fb8c782622ac4a1d0a6',
+            client_secret: 'fe62abf8e17b91d71e2c94c367af00e6ba7ba8e6',
+            code: this.state.code
+        }
+        console.log("GH : ", githubAuthObj)
+
+        /* axios.post('https://github.com/login/oauth/access_token', githubAuthObj, {
+            headers: {
+                'Accept': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+            .then((response) => {
+                console.log("RESPONSE : ", response)
+            }) */
+
+       /*  fetch('https://github.com/login/oauth/access_token', {
+            method: 'POST',
+            mode: 'cors',
+            headers : {
+                'Access-Control-Allow-Origin': '*',
+                'Accept': 'application/json',
+            },
+            body : {
+              githubAuthObj
+            }
+        })
+            .then((response) => {
+                console.log('rs : ', response)
+            }) */
+
     }
 
     render() {
         return (
             <div>
                 <h1>Redirect.....</h1>
-                <h1>Code : { this.state.code }</h1>
+                <h1>Code : {this.state.code}</h1>
             </div>
         )
     }
